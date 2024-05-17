6bc5aa94b52a636acb9fbadb66c633f22e70cbac
joshpait <joshpait@Ubuntu.myguest.virtualbox.org>
2024-05-16 20:36:53 -0700

    Added test js lines to display array in cards

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-const myLibrary = ["book1", "book2", "book3"];
+const myLibrary = [
+    { title: 'title', author: 'author' },
+    { title: 'title2', author: 'author2'}
+];
 
 function Book() {
-
+    title;
+    author;
+    numOfPages;
+    read;
 }
+
+
 
 function addBookToLibrary() {
 
 }
 
+function displayBooks(array) {
+    const container = document.getElementById('cardContainer');
+    container.innerHTML = '';
+
+    array.forEach (item => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const cardInnerHTML = `
+        <h3>${item.title}</h3>
+        <p>${item.author}</p>
+        `;
+
+        card.innerHTML = cardInnerHTML;
+        container.appendChild(card);
+    });
+}
+
+displayBooks(myLibrary);
