5c6940bb2012d0d83e2a40aa2520d5aa0e53f697
joshpait <joshpait@Ubuntu.myguest.virtualbox.org>
2024-05-16 21:09:17 -0700

    Deleted test js lines and added js lines for adding book through modal. Need to fix modal popup

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const myLibrary = [
-    { title: 'title', author: 'author' },
-    { title: 'title2', author: 'author2'}
-];
+const myLibrary = [];
+
+document.getElementById('openModalButton').addEventListener('click', openModal);
+document.getElementById('modalAddBookButton').addEventListener('click', addBook);
+document.querySelector('.close').addEventListener('click', closeModal);
+window.addEventListener('click', outsideClickCloseModal);
+
+function addModal() {
+    document.getElementById('bookModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('bookModal').style.display = 'none';
+}
+
+function outsideClickCloseModal() {
+    if (event.target == document.getElementById('bookModal')) {
+        closeModal();
+    }
+}
 
 function Book() {
     title;
@@ -11,27 +27,56 @@ function Book() {
 }
 
 
+function addBook() {
+    const title = document.getElementById('modalBookTitle').value;
+    const author = document.getElementById('modalBookAuthor').value;
+    const pages = document.getElementById('modalBookPages').value;
+    const isChecked = document.getElementById('modalBookRead').checked;
 
-function addBookToLibrary() {
+    if (!title || !author || !pages || !isChecked) {
+        alert('Please fill in all boxes.');
+        return;
+    }
 
+    const card = createElement('div');
+    card.className = 'card';
+
+    const cardInnerHTML = `
+    <h2>${title}</h2>
+    <h3>${author}</h3>
+    <p>${pages}</p>
+    <p>Read? ${isChecked ? 'checked' : 'unchecked'}</p>
+    `;
+    card.innerHTML = cardInnerHTML;
+
+    document.getElementById('cardContainer').appendChild(card);
+
+    myLibrary.push(card);
+
+    document.getElementById('modalCardTitle').value = '';
+    document.getElementById('modalCardContent').value = '';
+    document.getElementById('modalCardCheckbox').checked = false;
+
+    // Close the modal
+    closeModal();
 }
 
-function displayBooks(array) {
-    const container = document.getElementById('cardContainer');
-    container.innerHTML = '';
+// function displayBooks(array) {
+//     const container = document.getElementById('cardContainer');
+//     container.innerHTML = '';
 
-    array.forEach (item => {
-        const card = document.createElement('div');
-        card.className = 'card';
+//     array.forEach (item => {
+//         const card = document.createElement('div');
+//         card.className = 'card';
 
-        const cardInnerHTML = `
-        <h3>${item.title}</h3>
-        <p>${item.author}</p>
-        `;
+//         const cardInnerHTML = `
+//         <h3>${item.title}</h3>
+//         <p>${item.author}</p>
+//         `;
 
-        card.innerHTML = cardInnerHTML;
-        container.appendChild(card);
-    });
-}
+//         card.innerHTML = cardInnerHTML;
+//         container.appendChild(card);
+//     });
+// }
 
-displayBooks(myLibrary);
+// displayBooks(myLibrary);
