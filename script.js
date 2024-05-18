6fa33db7ae4e7c00fda0d42aeb7d70b2bf9efbe2
joshpait <joshpait@Ubuntu.myguest.virtualbox.org>
2024-05-17 20:14:19 -0700

    Fixed modal and added remove button for cards

--- script.js
+++ script.js
@@ -1,66 +1,84 @@
-const myLibrary = [];
+document.addEventListener('DOMContentLoaded', function() {
+    const myLibrary = [];
 
-document.getElementById('openModalButton').addEventListener('click', openModal);
-document.getElementById('modalAddBookButton').addEventListener('click', addBook);
-document.querySelector('.close').addEventListener('click', closeModal);
-window.addEventListener('click', outsideClickCloseModal);
+    document.getElementById('openModalButton').addEventListener('click', openModal);
+    document.getElementById('modalAddBookButton').addEventListener('click', addBook);
+    document.querySelector('.close').addEventListener('click', closeModal);
+    window.addEventListener('click', outsideClickCloseModal);
 
-function addModal() {
-    document.getElementById('bookModal').style.display = 'block';
-}
+    function openModal() {
+        document.getElementById('bookModal').style.display = 'block';
+    }
 
-function closeModal() {
-    document.getElementById('bookModal').style.display = 'none';
-}
+    function closeModal() {
+        document.getElementById('bookModal').style.display = 'none';
+    }
 
-function outsideClickCloseModal() {
-    if (event.target == document.getElementById('bookModal')) {
+    function outsideClickCloseModal(Event) {
+        if (Event.target == document.getElementById('bookModal')) {
+            closeModal();
+        }
+    }
+
+    function Book() {
+        title;
+        author;
+        numOfPages;
+        read;
+    }
+
+
+    function addBook() {
+        const title = document.getElementById('modalBookTitle').value;
+        const author = document.getElementById('modalBookAuthor').value;
+        const pages = document.getElementById('modalBookPages').value;
+        const isChecked = document.getElementById('modalBookRead').checked;
+
+        if (!title || !author || !pages) {
+            alert('Please fill in all boxes.');
+            return;
+        }
+
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const cardInnerHTML = `
+        <h2>${title}</h2>
+        <h3>by ${author}</h3>
+        <p>Number of pages: ${pages}</p>
+        <p>Read? ${isChecked ? 'Yes' : 'No'}</p>
+        <button class='remove-button'>Remove</button>
+        `;
+        card.innerHTML = cardInnerHTML;
+
+        document.getElementById('cardContainer').appendChild(card);
+
+        myLibrary.push(title);
+
+        card.querySelector('.remove-button').addEventListener('click', () => removeCard(card, title));
+
+        document.getElementById('modalBookTitle').value = '';
+        document.getElementById('modalBookAuthor').value = '';
+        document.getElementById('modalBookPages').value = '';
+        document.getElementById('modalBookRead').checked = false;
+
+        // Close the modal
         closeModal();
-    }
-}
 
-function Book() {
-    title;
-    author;
-    numOfPages;
-    read;
-}
-
-
-function addBook() {
-    const title = document.getElementById('modalBookTitle').value;
-    const author = document.getElementById('modalBookAuthor').value;
-    const pages = document.getElementById('modalBookPages').value;
-    const isChecked = document.getElementById('modalBookRead').checked;
-
-    if (!title || !author || !pages || !isChecked) {
-        alert('Please fill in all boxes.');
-        return;
+        console.log(myLibrary);
     }
 
-    const card = createElement('div');
-    card.className = 'card';
+    function removeCard(card, title) {
+        card.remove();
 
-    const cardInnerHTML = `
-    <h2>${title}</h2>
-    <h3>${author}</h3>
-    <p>${pages}</p>
-    <p>Read? ${isChecked ? 'checked' : 'unchecked'}</p>
-    `;
-    card.innerHTML = cardInnerHTML;
+        const cardIndex = myLibrary.indexOf(title);
+        if (cardIndex > -1) {
+            myLibrary.splice(cardIndex, 1);
+        }
 
-    document.getElementById('cardContainer').appendChild(card);
-
-    myLibrary.push(card);
-
-    document.getElementById('modalCardTitle').value = '';
-    document.getElementById('modalCardContent').value = '';
-    document.getElementById('modalCardCheckbox').checked = false;
-
-    // Close the modal
-    closeModal();
-}
-
+        console.log(myLibrary);
+    }
+});
 // function displayBooks(array) {
 //     const container = document.getElementById('cardContainer');
 //     container.innerHTML = '';
